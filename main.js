5ffa9ec51c3b7f8b2d1d5934e7f51cc31726ebea
Beko98 <99834191+Beko98@users.noreply.github.com>
2022-12-27 17:10:44 +0400

    finish

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//   .then((response) => response.json())
-//   .then((data) => {
 async function getData() {
   const response = await fetch("https://reqres.in/api/users?page=2");
   const data = await response.json();
@@ -15,3 +13,19 @@ fetch("https://jsonplaceholder.typicode.com/todos/1")
 fetch("https://fakerapi.it/api/v1/images?_width=380")
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+// Define a function that returns a Promise
+function getTheData() {
+  return new Promise((resolve, reject) => {
+    // Perform an asynchronous operation, such as a network request
+    setTimeout(() => {
+      // If the operation is successful, call the resolve function
+      resolve("Data retrieved");
+    }, 1000);
+  });
+}
+
+// Use the then() method to handle the Promise's resolved value
+getTheData().then((data) => {
+  console.log(data); // Output: 'Data retrieved'
+});
